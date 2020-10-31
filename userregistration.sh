2a27d78a03a8c0c2fc7fe6955b58b6bf58a29de2
baluguru <smkumarag@gmail.com>
2020-10-31 12:24:36 +0530

    Added last name validation

--- userregistration.sh
+++ userregistration.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-read -p "enter user first name : " firstName
+function validation(){
+input=$1
+pattern=$2
+flag=0
+while [ $flag -eq 0 ]
+do
+	if [[ $name =~ $pattern ]]
+	then
+		echo "it is valid"
+		flag=1
+	else
+		echo "it is not valid"
+		read input
+	fi
+done
+echo $input
+echo "enter firstName start with capital letter and has minimum 3 character"
+read firstName
 pattern="^[A-Z]{1}[a-z]{2}"
-if [[ $firstName =~ $pattern ]]
-then
-	echo "valid first Name"
-else
-	echo "Invalid first Name"
-fi
+validation $firstName $pattern
+
+echo "enter LastName start with capital letter and has minimum 3 character"
+read LastName
+validation $LastName $pattern
